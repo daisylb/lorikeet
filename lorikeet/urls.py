8d6943ecd4ceed5d8c2c6e17a2bb174bb438b832
Adam Brenecki <adam.brenecki@cmv.com.au>
2017-01-16 11:15:07 +1030

    Remove cart/ prefix from urls

--- lorikeet/urls.py
+++ lorikeet/urls.py
@@ -3,13 +3,13 @@ from django.conf.urls import url
 from . import api_views
 
 urlpatterns = [
-    url(r'^cart/$', api_views.CartView.as_view(), name='cart'),
-    url(r'^cart/(?P<id>\d+)/$', api_views.CartItemView.as_view(), name='cart-item'),
-    url(r'^cart/new/$', api_views.AddToCartView.as_view(), name='add-to-cart'),
-    url(r'^cart/new-address/$', api_views.NewAddressView.as_view(), name='new-address'),
-    url(r'^cart/payment-method/(?P<id>\d+)/$',
+    url(r'^$', api_views.CartView.as_view(), name='cart'),
+    url(r'^(?P<id>\d+)/$', api_views.CartItemView.as_view(), name='cart-item'),
+    url(r'^new/$', api_views.AddToCartView.as_view(), name='add-to-cart'),
+    url(r'^new-address/$', api_views.NewAddressView.as_view(), name='new-address'),
+    url(r'^payment-method/(?P<id>\d+)/$',
         api_views.PaymentMethodView.as_view(),
         name='payment-method'),
-    url(r'^cart/new-payment-method/$',
+    url(r'^new-payment-method/$',
         api_views.NewPaymentMethodView.as_view(), name='new-payment-method'),
 ]
