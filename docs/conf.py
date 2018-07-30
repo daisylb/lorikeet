7ff9767f333ff8446df259cd265cffbedbc04a0a
Adam Brenecki <adam@brenecki.id.au>
2018-07-30 10:21:47 +0930

    Fix package dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ author = 'Adam Brenecki'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.6'
+version = '0.1.7'
 # The full version, including alpha/beta/rc tags.
 release = version
 
